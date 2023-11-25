7bf02806097c10111f0c951cf2fdbbceeb91250e
Augusto Nunes <nunes.augusto19@gmail.com>
2023-11-25 01:03:34 -0300

    Fix typo and missing declarations

--- scgraphscrapper/track.py
+++ scgraphscrapper/track.py
@@ -11,31 +11,27 @@ class CollectTrack:
     def __init__(
         self, 
         client, 
-        user_id:str, 
+        track_id:str, 
         base_path:str, 
-        permalink:t.Union[None, str] = None,
     ):
-        if permalink:
-            self.user_id = client.get_user_by_username(permalink).id
-        else:
-            self.user_id = user_id
         
+        self.track_id = track_id
         self.base_path = base_path
-    
+        self.client = client
         self.map_of_func = {
             "albums": [self.client.get_track_albums, "album", "albums"],
             "playlists": [self.client.get_track_playlists, "playlist", "playlists"],
             "comments": [self.client.get_track_comments, "comment", "comments"],
-            "likers": [self.get_track_likers, "user", "likers"],
-            "reposters": [self.get_track_reposters, "user", "reposters"],
+            "likers": [self.client.get_track_likers, "user", "likers"],
+            "reposters": [self.client.get_track_reposters, "user", "reposters"],
         }
     
     async def get_(self, func, type_of_columns, dir):
-        list_of_elements = await list(func(self.user_id))
+        list_of_elements = await list(func(self.track_id))
         pd.DataFrame(
             [list(itemgetter(*features[type_of_columns]["list_columns"])(element.__dict__)) for element in list_of_elements],
             columns=features[type_of_columns]["list_columns"],
-        ).to_csv(os.path.join(self.base_path, dir, f"{self.user_id}.csv"), index=False)
+        ).to_csv(os.path.join(self.base_path, dir, f"{self.track_id}.csv"), index=False)
 
     
     async def get_data(self, *string_functions, all_data:bool=False):
